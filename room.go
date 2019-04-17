224d692618071853cc544a0f1a00e9251173af4b
WestEast1st <bhns1270132@gn.iwasaki.ac.jp>
2019-04-17 17:59:50 +0900

    [add]✨roomのsocket関連の関数を追加 # ==== Emojis ==== # 🐛  :bug: バグ修正 # 👍  :+1: 機能改善 # ✨  :sparkles: 部分的な機能追加 # 🎉  :tada: 盛大に祝うべき大きな機能追加 # ♻️   :recycle: リファクタリング # 🚿  :shower: 不要な機能・使われなくなった機能の削除 # 💚  :green_heart: テストやCIの修正・改善 # 👕  :shirt: Lintエラーの修正やコードスタイルの修正 # 🚀  :rocket: パフォーマンス改善 # 🆙  :up: 依存パッケージなどのアップデート # 🔒  :lock: 新機能の公開範囲の制限 # 👮  :cop: セキュリティ関連の改善 # ==== Format ==== # :emoji: Subject # # Commit body...
    # ==== The Seven Rules ====
    # 1. Separate subject from body with a blank line
    # 2. Limit the subject line to 50 characters
    # 3. Capitalize the subject line
    # 4. Do not end the subject line with a period
    # 5. Use the imperative mood in the subject line
    # 6. Wrap the body at 72 characters
    # 7. Use the body to explain what and why vs. how
    #
    # How to Write a Git Commit Message http://chris.beams.io/posts/git-commit/
    
    # for http://memo.goodpatch.co/2016/07/beautiful-commits-with-emojis/

--- room.go
+++ room.go
@@ -1,8 +1,15 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
 type room struct {
 	//foward is 他clientからのメッセージを保持するチャンネル
-	foward chan []byte
+	forward chan []byte
 	//join は chat room に join 後の client chanelを入れる
 	join chan *client
 	//leave はチャットルームから退室しようとしているチャンネル
@@ -10,6 +17,11 @@ type room struct {
 	//client には在室している全てのクライアントが保持
 	clients map[*client]bool
 }
+
+const (
+	socketBufferSize  = 1024
+	messageBufferSize = 256
+)
 
 func (r *room) rum() {
 	for {
@@ -21,7 +33,7 @@ func (r *room) rum() {
 			//退室
 			delete(r.clients, client)
 			close(client.send)
-		case msg := <-r.foward:
+		case msg := <-r.forward:
 			for client := range r.clients {
 				select {
 				case client.send <- msg:
@@ -32,5 +44,33 @@ func (r *room) rum() {
 				}
 			}
 		}
+	}
+}
+
+var upgrader = &websocket.Upgrader{ReadBufferSize: socketBufferSize, WriteBufferSize: socketBufferSize}
+
+func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	socket, err := upgrader.Upgrade(w, req, nil)
+	if err != nil {
+		log.Fatal("ServeHTTP:", err)
+		return
+	}
+	client := &client{
+		socket: socket,
+		send:   make(chan []byte, messageBufferSize),
+		room:   r,
+	}
+	r.join <- client
+	defer func() { r.leave <- client }()
+	go client.write()
+	client.read()
+}
+
+func newRoom() *room {
+	return &room{
+		forward: make(chan []byte),
+		join:    make(chan *client),
+		leave:   make(chan *client),
+		clients: make(map[*client]bool),
 	}
 }
