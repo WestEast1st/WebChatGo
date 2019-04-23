e081e0a921e0f85dcdaa11a9110870bdaba19915
WestEast1st <bhns1270132@gn.iwasaki.ac.jp>
2019-04-24 01:22:31 +0900

    Login時にアイコンの画像URLを取得

--- chat/auth.go
+++ chat/auth.go
@@ -65,7 +65,8 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 			log.Fatalln("ユーザー取得に失敗しました", provider, "-", err)
 		}
 		authCookieValue := objx.New(map[string]interface{}{
-			"name": user.Name(),
+			"name":       user.Name(),
+			"avater_url": user.AvatarURL(),
 		}).MustBase64()
 		http.SetCookie(w, &http.Cookie{
 			Name:  "auth",
