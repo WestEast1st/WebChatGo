acee98c8e32776c1d1255ae24968a2b5e6ed139a
WestEast1st <bhns1270132@gn.iwasaki.ac.jp>
2019-04-24 11:39:26 +0900

    Gravatarの運用を可能にする

--- chat/avatar.go
+++ chat/avatar.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"crypto/md5"
 	"errors"
+	"fmt"
+	"io"
+	"strings"
 )
 
 // ErrNoAvatarはAvatarインスタンスがアバターのURLを返すことができない場合に発生するエラー
@@ -23,6 +27,21 @@ func (_ AuthAvatar) GetAvatarURL(c *client) (string, error) {
 	if url, ok := c.userData["avatar_url"]; ok {
 		if urlstr, ok := url.(string); ok {
 			return urlstr, nil
+		}
+	}
+	return "", ErrNoAvatarURL
+}
+
+type GravatarAvatar struct{}
+
+var UseGravatar GravatarAvatar
+
+func (_ GravatarAvatar) GetAvatarURL(c *client) (string, error) {
+	if email, ok := c.userData["email"]; ok {
+		if emailstr, ok := email.(string); ok {
+			m := md5.New()
+			io.WriteString(m, strings.ToLower(emailstr))
+			return fmt.Sprintf("//www.gravatar.com/avatar/%x", m.Sum(nil)), nil
 		}
 	}
 	return "", ErrNoAvatarURL
