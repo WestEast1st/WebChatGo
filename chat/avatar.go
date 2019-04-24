aae24e3859dde33c44fbf69e9f680de1e21f83f5
WestEast1st <bhns1270132@gn.iwasaki.ac.jp>
2019-04-24 17:41:55 +0900

    必要な引数をChatUserに変更 & 簡素化

--- chat/avatar.go
+++ chat/avatar.go
@@ -14,7 +14,18 @@ var ErrNoAvatarURL = errors.New("char: アバターのURLを取得できませ�
 type Avatar interface {
 	// GetAvatarURLは使用されたクライアントのアバターのURLを返します
 	// 問題が発生した場合にはエラーを返します。特にURLを取得できなかった場合にはErrNoAvatarURLを返す。
-	GetAvatarURL(c *client) (string, error)
+	GetAvatarURL(u ChatUser) (string, error)
+}
+
+type TryAvatars []Avatar
+
+func (a TryAvatars) GetAvatarURL(u ChatUser) (string, error) {
+	for _, avatar := range a {
+		if url, err := avatar.GetAvatarURL(u); err == nil {
+			return url, nil
+		}
+	}
+	return "", ErrNoAvatarURL
 }
 
 type AuthAvatar struct{}
@@ -22,11 +33,10 @@ type AuthAvatar struct{}
 var UseAuthAvatar AuthAvatar
 
 // AvatarURLを取得
-func (_ AuthAvatar) GetAvatarURL(c *client) (string, error) {
-	if url, ok := c.userData["avatar_url"]; ok {
-		if urlstr, ok := url.(string); ok {
-			return urlstr, nil
-		}
+func (_ AuthAvatar) GetAvatarURL(u ChatUser) (string, error) {
+	url := u.AvatarURL()
+	if url != "" {
+		return url, nil
 	}
 	return "", ErrNoAvatarURL
 }
@@ -35,42 +45,23 @@ type GravatarAvatar struct{}
 
 var UseGravatar GravatarAvatar
 
-func (_ GravatarAvatar) GetAvatarURL(c *client) (string, error) {
-	/*
-		if email, ok := c.userData["email"]; ok {
-			if emailstr, ok := email.(string); ok {
-				m := md5.New()
-				io.WriteString(m, strings.ToLower(emailstr))
-				return fmt.Sprintf("//www.gravatar.com/avatar/%x", m.Sum(nil)), nil
-			}
-		}
-	*/
-	if userid, ok := c.userData["userid"]; ok {
-		if useridstr, ok := userid.(string); ok {
-			return "//www.gravatar.com/avatar/" + useridstr, nil
-		}
-	}
-	return "", ErrNoAvatarURL
+func (_ GravatarAvatar) GetAvatarURL(u ChatUser) (string, error) {
+	return "//www.gravatar.com/avatar/" + u.UniqueID(), nil
 }
 
 type FileSystemAvatar struct{}
 
 var UseFileSystemAvatar FileSystemAvatar
 
-func (_ FileSystemAvatar) GetAvatarURL(c *client) (string, error) {
-	if userID, ok := c.userData["userid"]; ok {
-		if userIdStr, ok := userID.(string); ok {
-			// return "/avatars/" + useridstr + ".jpg", nil
-			if files, err := ioutil.ReadDir("avatars"); err == nil {
-				fmt.Println(files)
-				for _, file := range files {
-					if file.IsDir() {
-						continue
-					}
-					if match, _ := filepath.Match(userIdStr+"*", file.Name()); match {
-						return "/avatars/" + file.Name(), nil
-					}
-				}
+func (_ FileSystemAvatar) GetAvatarURL(u ChatUser) (string, error) {
+	if files, err := ioutil.ReadDir("avatars"); err == nil {
+		fmt.Println(files)
+		for _, file := range files {
+			if file.IsDir() {
+				continue
+			}
+			if match, _ := filepath.Match(u.UniqueID()+"*", file.Name()); match {
+				return "/avatars/" + file.Name(), nil
 			}
 		}
 	}
